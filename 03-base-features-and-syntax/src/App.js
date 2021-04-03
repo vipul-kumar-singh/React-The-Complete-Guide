f8eea08a0eeb52c6c0af55fc6f587db3d2eb5435
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-04-04 00:51:36 +0530

    Understanding JSX

--- 03-base-features-and-syntax/src/App.js
+++ 03-base-features-and-syntax/src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+    // return (
+    //   <div className="App">
+    //     <h1>Hello World</h1>
+    //   </div>
+    // );
+
+    return React.createElement(
+      'div',
+      { className: 'App' },
+      React.createElement('h1', null, 'Hello World')
     );
   }
 }
