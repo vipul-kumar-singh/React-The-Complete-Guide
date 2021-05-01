b74f0afa08ee0e160cd6f025105b1c69c1b090c9
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-05-01 21:07:03 +0530

    Listening to User Input

--- 04-react-state-and-events/src/components/NewExpense/NewExpense.js
+++ 04-react-state-and-events/src/components/NewExpense/NewExpense.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import ExpenseForm from './ExpenseForm';
-import './NewExpense.css'
+import './NewExpense.css';
 
 const NewExpense = () => {
-    return <div className="new-expense">
-        <ExpenseForm />
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
     </div>
+  );
 };
 
 export default NewExpense;
