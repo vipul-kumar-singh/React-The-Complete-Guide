a2630af403b6ae53d32fb8b9509f8e9acb383f50
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-05-01 23:24:32 +0530

    Child-to-Parent Component Communication (Lifting State Up)

--- 04-react-state-and-events/src/components/NewExpense/ExpenseForm.js
+++ 04-react-state-and-events/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   /*
   const [userInput, setUserInput] = useState({
     enteredTitle: '',
@@ -58,7 +58,7 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
