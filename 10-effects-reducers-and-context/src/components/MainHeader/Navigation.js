ac6ba13f53b2d39bbfa9dcabb523f425875e28a9
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-05-15 22:53:56 +0530

    Building & Using a Custom Context Provider Component

--- 10-effects-reducers-and-context/src/components/MainHeader/Navigation.js
+++ 10-effects-reducers-and-context/src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = () => {
+const Navigation = (props) => {
   const ctx = useContext(AuthContext);
 
   return (
