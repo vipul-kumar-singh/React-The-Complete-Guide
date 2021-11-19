d6b6f6021c2e95445f60f495ab5bfa31f57d8e2a
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-11-19 18:55:38 +0530

    Making Items Removable

--- 11-food-order-app/src/components/Cart/Cart.js
+++ 11-food-order-app/src/components/Cart/Cart.js
@@ -8,12 +8,16 @@ import CartContext from '../../store/cart-context';
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const totalAmount = `$${Math.abs(cartCtx.totalAmount).toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
