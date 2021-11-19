002d2e9a8f7a7c1631ef4216c98a5d128f4bfa2c
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-11-19 17:26:52 +0530

    Renamed CartProvider

--- 11-food-order-app/src/App.js
+++ 11-food-order-app/src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
-import CartProvider from './store/CardProvider';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
