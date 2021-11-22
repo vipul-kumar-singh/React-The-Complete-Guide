a1adfedf601e59475318ab1b83ecd3707007264e
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-11-22 13:51:11 +0530

    Component Updates In Action

--- 12-how-react-works/src/App.js
+++ 12-how-react-works/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('App Running');
+
+  const toggleParagraphHandler = () => {
+    // setShowParagraph(!showParagraph); // not dependent on previous state
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph); // dependent on previous state
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph! </Button>
     </div>
   );
 }
