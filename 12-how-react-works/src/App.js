ef411502863a01e21d9dbc51418bdb7fcfb24464
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-11-22 16:11:57 +0530

    Optimizing with useMemo()

--- 12-how-react-works/src/App.js
+++ 12-how-react-works/src/App.js
@@ -1,34 +1,52 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
+import DemoList from './components/Demo/DemoList';
 import './App.css';
 
+// function App() {
+//   const [showParagraph, setShowParagraph] = useState(false);
+//   const [allowToggle, setAllowToggle] = useState(false);
+
+//   console.log('App Running');
+
+//   const toggleParagraphHandler = useCallback(() => {
+//     // setShowParagraph(!showParagraph); // not dependent on previous state
+//     if (allowToggle) {
+//       setShowParagraph((prevShowParagraph) => !prevShowParagraph); // dependent on previous state
+//     }
+//   }, [allowToggle]);
+
+//   const allowToggleHandler = () => {
+//     setAllowToggle(true);
+//   };
+
+//   return (
+//     <div className="app">
+//       <h1>Hi there!</h1>
+//       {/* {showParagraph && <p>This is new</p>} */}
+//       <DemoOutput show={showParagraph} />
+//       {/* <DemoOutput show={false} /> */}
+//       <Button onClick={allowToggleHandler}>Allow Toggling </Button>
+//       <Button onClick={toggleParagraphHandler}>Toggle Paragraph! </Button>
+//     </div>
+//   );
+// }
+
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState('My List');
 
-  console.log('App Running');
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
 
-  const toggleParagraphHandler = useCallback(() => {
-    // setShowParagraph(!showParagraph); // not dependent on previous state
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph); // dependent on previous state
-    }
-  }, [allowToggle]);
-
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  };
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      {/* {showParagraph && <p>This is new</p>} */}
-      <DemoOutput show={showParagraph} />
-      {/* <DemoOutput show={false} /> */}
-      <Button onClick={allowToggleHandler}>Allow Toggling </Button>
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph! </Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
