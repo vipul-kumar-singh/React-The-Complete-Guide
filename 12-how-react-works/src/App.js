faf04bc04c92df3721fa43408618d2c5a9e9a5b1
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-11-22 14:53:05 +0530

    A Closer Look At Child Component Re-Evaluation

--- 12-how-react-works/src/App.js
+++ 12-how-react-works/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
@@ -16,7 +17,8 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new</p>}
+      {/* {showParagraph && <p>This is new</p>} */}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph! </Button>
     </div>
   );
---
+++ 12-how-react-works/src/components/Demo/DemoOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MyParagraph from './Myparagraph';
+
+const DemoOutput = (props) => {
+  console.log('DemoOutput Running');
+  return <MyParagraph>{props.show ? 'This is new' : ''}</MyParagraph>;
+};
+
+export default DemoOutput;
