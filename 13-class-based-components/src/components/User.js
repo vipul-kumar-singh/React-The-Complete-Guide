d5244d34feb222e1682a741c0f2d34dc6beabbd5
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-11-22 18:16:38 +0530

    Lifecycle Methods In Action

--- 13-class-based-components/src/components/User.js
+++ 13-class-based-components/src/components/User.js
@@ -7,6 +7,10 @@ import classes from './User.module.css';
 // };
 
 class User extends Component {
+  componentWillUnmount() {
+    console.log('User will unmount!');
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
