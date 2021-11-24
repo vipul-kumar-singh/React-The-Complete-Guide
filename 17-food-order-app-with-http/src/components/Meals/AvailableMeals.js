2709e77ce0a9777241620f431fe0dca884bc07ec
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-11-24 14:36:09 +0530

    Handling the Loading State

--- 17-food-order-app-with-http/src/components/Meals/AvailableMeals.js
+++ 17-food-order-app-with-http/src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -27,9 +28,18 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
