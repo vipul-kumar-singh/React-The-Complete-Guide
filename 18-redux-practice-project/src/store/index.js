58479b639c8ca65c4d40d06a83acf798d169efb6
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-04-10 00:46:11 +0530

    How To Work With Redux State Correctly

--- 18-redux-practice-project/src/store/index.js
+++ 18-redux-practice-project/src/store/index.js
@@ -4,6 +4,7 @@ const initialState = { counter: 0, showCounter: true };
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
+    // state.counter++; // Never do this
     return {
       counter: state.counter + 1,
       showCounter: state.showCounter,
