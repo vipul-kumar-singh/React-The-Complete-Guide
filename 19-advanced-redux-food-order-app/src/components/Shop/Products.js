e4fabbd720e1e99d44f27cb695ea9536e9b50ca3
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-04-10 20:31:13 +0530

    Refresher / Practice: Part 2/2

--- 19-advanced-redux-food-order-app/src/components/Shop/Products.js
+++ 19-advanced-redux-food-order-app/src/components/Shop/Products.js
@@ -1,16 +1,25 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  { id: 'p1', price: 6, title: 'Pizza', description: 'Freshly baked pizza' },
+  { id: 'p2', price: 5, title: 'Pasta', description: 'Freshly cooked pasta' },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
