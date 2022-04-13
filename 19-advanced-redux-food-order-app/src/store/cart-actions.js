5f0741148ccbeb11fc7eb1b7adeb04a19b0b9807
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-04-14 00:19:44 +0530

    Finalizing the Fetching Logic

--- 19-advanced-redux-food-order-app/src/store/cart-actions.js
+++ 19-advanced-redux-food-order-app/src/store/cart-actions.js
@@ -18,7 +18,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -46,7 +51,10 @@ export const sendCartData = (cart) => {
         'https://react-http-5e032-default-rtdb.firebaseio.com/advanced-redux/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
