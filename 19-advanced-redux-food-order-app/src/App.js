b8c657fd3037ba87762d916f6dc9c6c6c789abfa
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-04-13 23:00:12 +0530

    Using useEffect with Redux

--- 19-advanced-redux-food-order-app/src/App.js
+++ 19-advanced-redux-food-order-app/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,17 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      'https://react-http-5e032-default-rtdb.firebaseio.com/advanced-redux/cart.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
 
   return (
     <Layout>
