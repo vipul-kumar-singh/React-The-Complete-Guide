2b733c1ede13ece18478e8e41c39648a1ced7323
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-04-14 20:07:48 +0530

    Adding a 'Not Found' Page

--- 20-practice-project/src/pages/QuoteDetail.js
+++ 20-practice-project/src/pages/QuoteDetail.js
@@ -21,7 +21,6 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <p>{params.quoteId}</p>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
