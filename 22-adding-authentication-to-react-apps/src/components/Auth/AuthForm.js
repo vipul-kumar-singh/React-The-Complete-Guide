cea09abfc12a0badfe5b9ca38ed6f20a01696daa
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-04-15 20:24:05 +0530

    Showing Feedback To The User

--- 22-adding-authentication-to-react-apps/src/components/Auth/AuthForm.js
+++ 22-adding-authentication-to-react-apps/src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -19,6 +20,8 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     // optional: Add validation
+
+    setIsLoading(true);
 
     if (isLogin) {
     } else {
@@ -36,10 +39,15 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false);
         if (res.ok) {
         } else {
           res.json().then((data) => {
-            console.log(data);
+            let errorMessage = 'Authentication Failed!';
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
           });
         }
       });
@@ -64,7 +72,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
