d058aa496dbd721a6714dacc3033f9d94c7abb62
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-05-02 18:50:02 +0530

    Customizing CSS Classnames

--- 23-animating-react-apps/src/components/Modal/Modal.js
+++ 23-animating-react-apps/src/components/Modal/Modal.js
@@ -15,7 +15,13 @@ const modal = (props) => {
       timeout={animationTiming}
       mountOnEnter
       unmountOnExit
-      classNames="fade-slide"
+      //   classNames="fade-slide"
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed',
+      }}
     >
       <div className="Modal">
         <h1>A Modal</h1>
