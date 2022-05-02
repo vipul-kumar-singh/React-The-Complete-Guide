62f001be4eac306fd3e386e3992a19e7329d2cfc
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-05-02 18:13:21 +0530

    Using the Transition Component

--- 23-animating-react-apps/src/App.js
+++ 23-animating-react-apps/src/App.js
@@ -52,12 +52,15 @@ class App extends Component {
             />
           )}
         </Transition>
-        {this.state.modalIsOpen ? (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        ) : null}
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} />
-        ) : null}
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => <Modal show={state} closed={this.closeModal} />}
+        </Transition>
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
