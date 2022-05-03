d7ec1ed32007d647bb6fc48fae7ed2a01996e985
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-05-03 14:19:42 +0530

    Alternative: Using the Context API

--- 24-replace-redux-with-react-hooks/src/components/Products/ProductItem.js
+++ 24-replace-redux-with-react-hooks/src/components/Products/ProductItem.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
 import { toggleFav } from '../../store/actions/products';
 
-const ProductItem = props => {
-  const dispatch = useDispatch();
-
+const ProductItem = (props) => {
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    // dispatch(toggleFav(props.id));
   };
 
   return (
--- 24-replace-redux-with-react-hooks/src/store/reducers/products.js
+++ 24-replace-redux-with-react-hooks/src/store/reducers/products.js
@@ -6,44 +6,44 @@ const initialState = {
       id: 'p1',
       title: 'Red Scarf',
       description: 'A pretty red scarf.',
-      isFavorite: false
+      isFavorite: false,
     },
     {
       id: 'p2',
       title: 'Blue T-Shirt',
       description: 'A pretty blue t-shirt.',
-      isFavorite: false
+      isFavorite: false,
     },
     {
       id: 'p3',
       title: 'Green Trousers',
       description: 'A pair of lightly green trousers.',
-      isFavorite: false
+      isFavorite: false,
     },
     {
       id: 'p4',
       title: 'Orange Hat',
       description: 'Street style! An orange hat.',
-      isFavorite: false
-    }
-  ]
+      isFavorite: false,
+    },
+  ],
 };
 
 const productReducer = (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_FAV:
       const prodIndex = state.products.findIndex(
-        p => p.id === action.productId
+        (p) => p.id === action.productId
       );
       const newFavStatus = !state.products[prodIndex].isFavorite;
       const updatedProducts = [...state.products];
       updatedProducts[prodIndex] = {
         ...state.products[prodIndex],
-        isFavorite: newFavStatus
+        isFavorite: newFavStatus,
       };
       return {
         ...state,
-        products: updatedProducts
+        products: updatedProducts,
       };
     default:
       return state;
